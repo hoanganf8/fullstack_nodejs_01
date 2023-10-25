5a4ece1047ca0a2b8b07a9b9651adcf0a6f64b58
Hoàng An F8 <hoangan@fullstack.edu.vn>
2023-10-25 19:37:18 +0700

    Update code

--- 8. hocreact/react01/src/App.jsx
+++ 8. hocreact/react01/src/App.jsx
@@ -1,7 +1,33 @@
 import Header from "./Components/Header";
-
+import React from "react";
 function App() {
-  return <Header />;
+  const h1 = React.createElement(
+    "h1",
+    {
+      id: "title",
+      className: "title",
+      style: {
+        color: "red",
+        fontStyle: "italic",
+      },
+      onClick: (e) => {
+        console.log("Click me", e);
+      },
+    },
+    "Hello F8",
+  );
+  const h2 = React.createElement("h2", {}, "Học React không khó");
+
+  const div = React.createElement(
+    "div",
+    {
+      className: "container",
+    },
+    h1,
+    h2,
+  );
+
+  return div;
 }
 
 export default App;
--- 8. hocreact/react01/src/main.jsx
+++ 8. hocreact/react01/src/main.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(App());
 
 //main: nhận request từ index.html => gọi component App => Render nội dung component App vào thẻ div root
+
+/*
+Bài tập: Tạo thẻ ul, li -> Chèn vào sau thẻ h1
+<ul class="menu">
+  <li>Item 1</li>
+  <li>Item 2</li>
+  <li>Item 3</li>
+</ul>
+*/
