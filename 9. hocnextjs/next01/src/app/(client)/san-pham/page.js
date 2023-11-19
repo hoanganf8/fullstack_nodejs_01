7b08d69571112a138ad0a8437fbb400fe7c2f40a
Hoàng An F8 <hoangan@fullstack.edu.vn>
2023-11-19 23:35:07 +0700

    Buoi 48

---
+++ 9. hocnextjs/next01/src/app/(client)/san-pham/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Danh sách sản phẩm",
+};
+
+const Products = () => {
+  return (
+    <div>
+      <h1>Danh sách sản phẩm</h1>
+    </div>
+  );
+};
+
+export default Products;
